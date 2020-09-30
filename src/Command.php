ccd6b742b9e25467e0b0967520d21631f6a215e5
Marijn Ophorst <marijn@monomelodies.nl>
2020-09-30 17:28:15 +0200

    improve inner workings; show line numbers etc

--- src/Command.php
+++ src/Command.php
@@ -9,12 +9,46 @@ class Command extends Cliff\Command
 {
     public array $source = [];
 
+    private array $files = [];
+
     public function __invoke(string $cssDir) : void
     {
         if (!$this->source) {
             $this->source = [$cssDir];
         }
+        foreach ($this->source as $source) {
+            $this->walkSourceDir($source);
+        }
         $this->walkCssDir($cssDir);
+    }
+
+    private function walkSourceDir(string $dir) : void
+    {
+        $d = dir($dir);
+        while (false !== ($entry = $d->read())) {
+            if (in_array($entry, ['.', '..'])) {
+                continue;
+            }
+            if (is_dir("$dir/$entry")) {
+                $this->walkSourceDir("$dir/$entry");
+            } elseif (preg_match("@\.(css|scss|less)$@", $entry)) {
+                continue;
+            } elseif (preg_match('@\.jsx?$@', $entry)) {
+                $code = file_get_contents("$dir/$entry");
+                preg_match_all('@[\.#][\w-]+@', $code, $mentions);
+                $this->files["$dir/$entry"] = $mentions[0];
+            } else {
+                $code = file_get_contents("$dir/$entry");
+                preg_match_all('@(id|class)="(.*?)"@', $code, $mentions);
+                $this->files["$dir/$entry"] = [];
+                foreach ($mentions[2] as $i => $mention) {
+                    $parts = preg_split("@\s+@", $mention);
+                    foreach ($parts as $selector) {
+                        $this->files["$dir/$entry"][] = ($mentions[1][$i] == 'id' ? '#' : '.').$selector;
+                    }
+                }
+            }
+        }
     }
 
     private function walkCssDir(string $dir) : void
@@ -28,58 +62,42 @@ class Command extends Cliff\Command
                 $this->walkCssDir("$dir/$entry");
             } elseif (preg_match("@\.(css|scss|less)$@", $entry)) {
                 $css = file_get_contents("$dir/$entry");
-                preg_match_all("@[\.#]\w+@", $css, $matches, PREG_SET_ORDER);
-                $rules = [];
-                foreach ($matches as $match) {
-                    $rules[] = $match[0];
-                }
-                $rules = array_unique($rules);
-                foreach ($rules as $rule) {
-                    $occurencs = false;
-                    foreach ($this->source as $sourcedir) {
-                        if ($this->walkSourceDir($sourcedir, $rule)) {
-                            continue 2;
-                        }
+                $lines = explode("\n", $css);
+                foreach ($lines as $lineno => $line) {
+                    $rules = [];
+                    // If we're not opening a block of rules, ignore by default.
+                    if (!preg_match("@{\$@", $line)) {
+                        continue;
                     }
-                    echo Ansi::tagsToColors("In <red>$dir/$entry<reset>: <green>$rule<reset>\n");
+                    // Adjacent child etc. selectors are taking it too far.
+                    $line = preg_replace('@[+~>&]@', ' ', $line);
+                    $_rules = preg_split("@\s+@", trim(substr($line, 0, -1)));
+                    array_walk($_rules, function (&$rule) use (&$rules) {
+                        // Ignore pseudo-classes.
+                        $rule = preg_replace('@:.*$@', '', $rule);
+                        preg_match_all('@([\.#][\w-]+)@', $rule, $parts);
+                        $rules = array_merge($rules, $parts[0]);
+                    });
+                    $rules = array_filter(array_unique($rules), function ($rule) {
+                        // Ignore empty selectors, or any selectors that do not contain an id or class name.
+                        return strlen($rule) && (strpos($rule, '.') !== false || strpos($rule, '#') !== false);
+                    });
+                    foreach ($rules as $rule) {
+                        foreach ($this->files as $file) {
+                            if (in_array($rule, $file)) {
+                                continue 2;
+                            }
+                        }
+                        echo Ansi::tagsToColors("In <red>$dir/$entry<reset>: <green>$rule<reset> on line <bold>$lineno<reset>\n");
+                    }
                 }
             }
         }
     }
 
-    private function walkSourceDir(string $dir, string $rule) : bool
+    private function stripType(string &$rule) : void
     {
-        $d = dir($dir);
-        while (false !== ($entry = $d->read())) {
-            if (in_array($entry, ['.', '..'])) {
-                continue;
-            }
-            if (is_dir("$dir/$entry")) {
-                if ($this->walkSourceDir("$dir/$entry", $rule)) {
-                    return true;
-                }
-            } elseif (preg_match("@\.(css|scss|less)$@", $entry)) {
-                continue;
-            } elseif (preg_match('@\.jsx?$@', $entry)) {
-                $code = file_get_contents("$dir/$entry");
-                if (strpos($code, $rule) !== false) {
-                    return true;
-                }
-            } else {
-                $code = file_get_contents("$dir/$entry");
-                if (substr($rule, 0, 1) == '#') {
-                    if (strpos($code, 'id="'.substr($rule, 1).'"') !== false) {
-                        return true;
-                    }
-                } else {
-                    $class = substr($rule, 1);
-                    if (preg_match('@["\s]'.$class.'["\s]@', $code)) {
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
+        $rule = substr($rule, 1);
     }
 }
 
